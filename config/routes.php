ee76b06f872dd7500e3cce53caa1f1d9e4021d7d
Marc-Andre Dion <tdion.ma@gmail.com>
2016-02-19 15:51:25 -0500

    Fix .gitignore Fix merge conflicts

--- config/routes.php
+++ config/routes.php
@@ -47,7 +47,9 @@ Router::scope('/', function ($routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Products', 'action' => 'product', 'product']);
+    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+
+    $routes->connect('/product', ['controller' => 'Products', 'action' => 'product', 'product']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
