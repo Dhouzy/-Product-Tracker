5aa2a33c12860264813d707d922e24ca3f436e00
Hippolyte <hippolyte.glaus@gmail.com>
2016-03-11 10:06:13 -0500

    update for the new bd V7

--- src/Shell/PriceUpdateShell.php
+++ src/Shell/PriceUpdateShell.php
@@ -7,35 +7,36 @@
  */
 
 namespace App\Shell;
+
+use App\Core\Amazon\AmazonHelper;
 use Cake\Console\Shell;
 use Cake\ORM\TableRegistry;
 use DateTime;
 use DateTimeZone;
 
+
 class PriceUpdateShell extends Shell
 {
+    private $products;
+    private $prices;
 
     public function main(){
         $this->out('Daily update started');
 
-        $products = TableRegistry::get('products');
-        $prices = TableRegistry::get('prices');
-        $query = $products->find();
+        $this->products = TableRegistry::get('products');
+
+        $query = $this->products->find()->contain(['Prices'])->all();
 
         foreach ($query as $row) {
             $this->out($row->name);
-
-            //get the prices column
-            $product_price_id = $row->last_price_id;
-            $row_price = $prices->get($product_price_id);
-            $this->out($row_price->get("price"));
+            $this->out($row->price->price);
 
             //compare 2 dates
-            $interval = $this->compareTime($row_price);
+            $interval = $this->compareTime($row->price->date);
 
             $this->out($interval->format('%R%a days'));
             if($interval->d > 0){
-                $this->updatePrice($row);
+//                $this->updatePrice($row);
             }
         }
         $this->out('Daily update ended');
@@ -43,14 +44,15 @@ class PriceUpdateShell extends Shell
 
     function updatePrice($product_row){
         $this->out("updating price");
-        $prices = TableRegistry::get('prices');
-        $products = TableRegistry::get('prices');
+//        $prices = TableRegistry::get('prices');
 
         //call service api / adapter_api
         $new_price = null;
+        $amazon = new AmazonHelper();
+
 
         //create new price row
-        $query = $prices->query();
+        $query = $this->prices->query();
         $query->insert(['date', 'price', 'product_id'])
             ->values([
                 'date' => $new_price->date,
@@ -70,19 +72,17 @@ class PriceUpdateShell extends Shell
         $query->select('LAST_INSERT_ID()');
 
         //update last_price id of product_row
-        $query = $products->query();
+        $query = $this->products->query();
         $query->update()
             ->set(['last_price_id' => $last_insert_id])
             ->where(['id' => $product_row->id])
             ->execute();
     }
 
-    private function compareTime($row_price){
+    private function compareTime($last_price_update){
         $now = new DateTime(null, new DateTimeZone('America/Toronto'));
 //        echo $now->format('Y-m-d H:i:s');    // MySQL datetime format
 
-        $last_price_update = $row_price->get("date");
         return $interval = $last_price_update->diff($now);
     }
-
 }
